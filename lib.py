0afac96465ac6847af543bbb0d53b8dde60cc593
NailMukhametshin <nail.zubr@gmail.com>
2019-02-10 09:41:01 +0300

    refactoring: early exit

--- lib.py
+++ lib.py
@@ -10,14 +10,17 @@ def is_creditable(age, salary):
     False
     """
 
-
-
-
     min_age = 21
     max_age = 60
     min_salary = 30_000
 
-    if min_age <= age < max_age and salary >= min_salary:
-        return True
-    else:
+    if age < min_age:
         return False
+
+    if age > max_age:
+        return False
+
+    if salary < min_salary:
+        return False
+
+    return True
