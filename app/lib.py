217a93f67eba8cf6bdbb596d0c825b2033ec6eeb
NailMukhametshin <nail.zubr@gmail.com>
2019-02-10 11:03:58 +0300

    coverege added

--- app/lib.py
+++ app/lib.py
@@ -1,13 +1,9 @@
 def is_creditable(age, salary):
     """
-    >>> is_creditable(30, 40_000)
-    True
 
-    >>> is_creditable(70, 40_000)
+    >>> is_creditable(20, 40_000)
     False
 
-    >>> is_creditable(30, 10_000)
-    False
     """
 
     min_age = 21
